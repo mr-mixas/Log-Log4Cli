58b13cbdd10c221a2761be6767075df902539b6a
mr-mixas <mixas.sr@gmail.com>
2016-05-13 14:23:14 +0300

    don't check term status every time, log_fd() appeared

--- lib/Log/Log4Cli.pm
+++ lib/Log/Log4Cli.pm
@@ -6,8 +6,8 @@ use warnings;
 use Term::ANSIColor "colored";
 use parent "Exporter";
 
-our $VERSION = '0.03'; # Don't forget to change in pod below
-our @EXPORT = qw(die_fatal die_info log_fatal log_error log_warn log_info log_debug log_trace);
+our $VERSION = '0.04'; # Don't forget to change in pod below
+our @EXPORT = qw(die_fatal die_info log_fd log_fatal log_error log_warn log_info log_debug log_trace);
 
 our $C = {
     FATAL => 'bold red',
@@ -17,14 +17,16 @@ our $C = {
     DEBUG => 'blue',
     TRACE => 'magenta'
 };
-our $F = \*STDERR;
 our $L = 0;
 our $N = undef;
+
+my $F = *STDERR;    # descriptor
+my $T = -t $F;      # term/noterm flag
 
 sub _pfx($) {
     my ($S, $M, $H, $d, $m, $y) = localtime(time);
     my $pfx = sprintf "[%04i-%02i-%02i %02i:%02i:%02i %i %5s] ", $y + 1900, $m + 1, $d, $H, $M, $S, $$, $_[0];
-    return (-t $F ? colored($pfx, $C->{$_[0]}) : $pfx) . (($N or $L > 4) ? join(":", (caller(1))[1,2]) . " " : "");
+    return ($T ? colored($pfx, $C->{$_[0]}) : $pfx) . (($N or $L > 4) ? join(":", (caller(1))[1,2]) . " " : "");
 }
 
 sub die_fatal(;$;$) {
@@ -50,6 +52,14 @@ sub log_info(&)  { print $F _pfx('INFO'),  $_[0]->($_), "\n" if $L >  1 }
 sub log_debug(&) { print $F _pfx('DEBUG'), $_[0]->($_), "\n" if $L >  2 }
 sub log_trace(&) { print $F _pfx('TRACE'), $_[0]->($_), "\n" if $L >  3 }
 
+sub log_fd(;$) {
+    if (@_) {
+        $F = shift;
+        $T = -t $F;
+    }
+    return $F;
+}
+
 1;
 
 __END__
@@ -60,16 +70,16 @@ Log::Log4Cli -- Lightweight perl logger for command line tools
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =head1 SYNOPSIS
 
     Log::Log4Cli;
 
     $Log::Log4Cli::C->{DEBUG} = 'green';      # redefine color
-    $Log::Log4Cli::F = \*STDOUT               # print to STDOUT (STDERR by default)
     $Log::Log4Cli::L = 5;                     # set loglevel
     $Log::Log4Cli::N = 1;                     # force file:line indicators (also enables if loglevel > 4)
+    log_fd(*STDOUT);                          # print to STDOUT (STDERR by default)
 
     log_error { "blah-blah, it's an error" };
     log_trace { "Guts:\n" . Dumper $struct }; # Dumper will be called only when TRACE level enabled
